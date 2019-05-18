b0fa74f55cbba0383966400c41c50a971d52c584
djohansson@velvetsofa.com <djohansson>
2019-05-17 18:38:30 -0700

    end of day daywork

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,14 +1,47 @@
+require "byebug"
+
 class PolyTreeNode
 
-def initialize(value)
-  @value = value
-  @parent = nil
-  @children = []
-end
+    attr_accessor :children, :parent, :value
 
-def parent=(node)
-  @parent = node
+  def inspect
+    "value:#{@value} parent:#{@parent} children:#{@children}"
+  end
 
-end
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent=(node)
+    if self.parent
+      self.parent.children.delete(self)
+    end
+      
+    @parent = node
+    unless parent.nil?
+      @parent.children << self unless @parent.children.include?(self)
+    end
+
+  end
+
+  def add_child(child_node)
+    if !children.include?(child_node)
+      @children << child_node
+      child_node.parent = self
+    end
+  end
+
+  def remove_child(child_node)
+    
+    
+    if children.include?(child_node)
+      @children.delete(child_node)
+      child_node.parent = nil
+    else
+      raise "Child does not exist!"
+    end
+  end
 
 end
